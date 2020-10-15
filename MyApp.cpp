3c4bb3c6cd15989286b5f9de64356346e90cd527
Amogh-01-Koushik <72942651+Amogh-01-Koushik@users.noreply.github.com>
2020-10-16 00:14:00 +0530

    My App file

---
+++ MyApp.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main()
+{
+	string user_choice;
+	do
+	{
+		vector <int> vec;
+		cin >> user_choice;
+
+		if (user_choice == "P" || user_choice == "p")
+		{
+			for ( auto a : vec)			}
+			{
+				/* code */
+			}
+		}
+
+
+    }while (user_choice != "Q" || )
+	return 0;
+}
